85e113690dc02943a8ec553bb0a78eee1ab8e03c
ssmahim01 <mahimzayn666@gmail.com>
2024-11-19 17:42:13 +0600

    Updated MyProfile page and Added discountBG image

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,39 +1,44 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/Provider";
+import coverBG from "/src/assets/discountBG.jpg";
 
 const MyProfile = () => {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className="py-16 lg:w-3/5 w-11/12 mx-auto flex md:flex-row flex-col justify-between">
-      <div className="md:w-1/2 mx-auto md:border-r-2 border-gray-300 md:mr-20 md:mb-0 mb-12">
+    <div className="py-16 lg:w-3/5 w-11/12 mx-auto flex md:flex-row flex-col justify-between items-center">
+      <div className="md:w-1/2 mx-auto md:mb-0 mb-12">
         <figure>
           <img
-            className="md:w-1/2 w-11/12 rounded-full border-2 border-gray-300"
-            src={user.photoURL}
-            alt={user.displayName}
+            className="w-11/12 mx-auto lg:h-80 h-64 rounded-xl border-2 border-gray-200 bg-base-100 shadow-md"
+            src={coverBG}
+            alt="Cover"
           />
         </figure>
 
-        <h2 className="lg:text-3xl text-2xl text-gray-800 font-bold mt-4">
+        <h2 className="text-3xl text-center text-gray-800 font-bold mt-6">
           Welcome {user?.displayName}
         </h2>
-
         </div>
 
-        <div className="md:w-1/2 mx-auto card bg-base-100 w-11/12 shadow-md border-2 border-gray-300">
+        <div className="divider divider-horizontal divider-start"></div>
+
+        <div className="md:w-1/2 mx-auto card bg-base-100 w-11/12 shadow-md border-2 border-gray-300 p-4 ml-4">
           <figure>
             <img
               src={user?.photoURL}
               alt={user?.displayName}
-              className="w-full lg:h-64 h-52 rounded-t-lg"
+              className="w-3/5 border-2 border-gray-300 rounded-full"
             />
           </figure>
-          <div className="space-y-2 p-5 text-center">
-            <h2 className="lg:text-xl text-lg text-gray-800 font-bold">
+
+          <div className="divider"></div>
+
+          <div className="space-y-2 text-center">
+            <h2 className="lg:text-xl text-lg text-gray-700 font-bold">
               <span className="text-gray-950">User Name:</span> {user?.displayName}
             </h2>
-            <p className="lg:text-lg text-gray-800 font-bold">
+            <p className="lg:text-lg text-gray-700 font-bold">
               <span className="text-gray-900">User Email:</span> {user?.email}
             </p>
 
